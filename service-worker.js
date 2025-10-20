ae036680403d08386300232ea527db4a196a6367
Jane <janeemmanuel247@gmail.com>
2025-10-20 09:26:43 -0700

    Refactor service worker event listeners

--- service-worker.js
+++ service-worker.js
@@ -9,34 +9,18 @@ const urlsToCache = [
   "./icons/icon-512.png"
 ];
 
-// Install: cache app shell
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(urlsToCache);
-    })
-  );
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)));
   self.skipWaiting();
 });
 
-// Activate: remove old caches
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.filter(key => key !== CACHE_NAME)
-            .map(key => caches.delete(key))
-      );
-    })
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
 
-// Fetch: serve from cache, fallback to network
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener("fetch", (event) => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
 });
